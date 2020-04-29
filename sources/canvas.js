29602a4711e3cd924ab85d8a6c3b545505b53b91
ionutantonesei <ionutantonesei@yahoo.com>
2020-04-29 17:43:33 +0300

    Modificari canvas

--- sources/canvas.js
+++ sources/canvas.js
@@ -1,9 +1,17 @@
 const canvas = document.getElementById('interactiv');
 const ctx = interactiv.getContext('2d');
 
+ctx.beginPath();
+ctx.lineWidth = 5;
+const centru_X = 150;
+const centru_Y = 200;
+ctx.arc(centru_X,centru_Y,100,0,Math.PI*2);
+ctx.stroke();
+
+ctx.lineWidth = 1;
 ctx.strokeStyle = "hsl(120, 100%, 10%)";
-ctx.strokeRect (10,15,200,300);
+ctx.strokeRect (10,15,280,380);
 
 ctx.fillStyle = 'black';
 ctx.font = '30px Arial';
-ctx.fillText ('Un test' , 15 , 170);
+ctx.fillText ('UN OM' , 15 , 170);
