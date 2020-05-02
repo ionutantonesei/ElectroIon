3cfcb2dcca3bc7190b6ead01c9a5e2719be64bfd
ionutantonesei <ionutantonesei@yahoo.com>
2020-05-02 11:30:55 +0300

    Created test.html page and created quiz script. NOT FINAL

---
+++ sources/test.js
@@ -0,0 +1,18 @@
+let buton_intrebare_1 = document.getElementById('buton_intrebare_1');
+const intrebare = document.getElementById('intrebare_1');
+const variante = document.getElementById('variante_1');
+
+buton_intrebare_1.addEventListener('click',function(){
+    let intrare_raspuns_1 = document.getElementById('raspuns_1').checked;
+    let intrare_raspuns_2 = document.getElementById('raspuns_2').checked;
+    let intrare_raspuns_3 = document.getElementById('raspuns_3').checked;
+
+    if(intrare_raspuns_1 == true && intrare_raspuns_2 == false && intrare_raspuns_3 == false){
+        alert('CORECT');
+    }
+    else{
+        alert('GRESIT');
+    }
+    intrebare.remove();
+    variante.remove();
+})
